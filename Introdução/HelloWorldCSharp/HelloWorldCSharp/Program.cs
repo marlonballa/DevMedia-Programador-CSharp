ca2a094a19e5adb44a57c39cca972019d3784f3d
marlonballa <marlonballa07@gmail.com>
2022-07-26 21:22:23 -0300

    Hello World em C#

---
+++ Introdução/HelloWorldCSharp/HelloWorldCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿
+static void Main()
+{
+    HelloWorld hello = new HelloWorld();
+    hello.SetHello("Hello World");
+    Console.WriteLine(hello.GetHello());
+    Console.ReadLine();
+}
+
+public class HelloWorld
+{
+    private string _hello;
+
+    public void SetHello(string hello)
+    {
+        _hello = hello;
+    }
+    public string GetHello()
+    {
+        return _hello;
+    }
+}
+
+
+using System;
+
+public class Carros
+{
+    string _carro;
+
+    public ____ SetCarro(string carro)
+{
+    _carro = carro;
+}
+
+public string GetCarro____
+{
+        return _carro;
+}
+}
